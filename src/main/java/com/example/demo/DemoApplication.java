6f4cf9b5447171e5ebf2f5a11c718a42c3428ac3
xpx951201 <xpx951201@163.com>
2019-05-28 16:54:10 +0800

    修改主启动类

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 主启动类
+ * @author xiapeixuan
+ * @date 2019.05
+ * @version 1.0
+ */
 @SpringBootApplication
 public class DemoApplication {
 
